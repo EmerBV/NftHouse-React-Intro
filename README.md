5e4855f2eb5100dbc6e5bf8a6253741ab0471ee3
Emerson BV <emersmc@hotmail.com>
2022-03-07 21:02:36 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+live-server --port=8000
+
+http://127.0.0.1:8000/api/nfts?userId=1
---
+++ nfthouse/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    '*.{html,js}',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: '1124px',
+        xl: '1124px',
+        "2xl": "1124px",
+      },
+    },
+  },
+  plugins: [],
+}
